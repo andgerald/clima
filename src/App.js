393b9ad0f2908380febbe6cc1b06a488a2a785ff
andgeral <geraldinebecerra93@gmal.com>
2021-11-28 23:22:00 -0300

    formulario , estilos globales

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-
+import { Fragment } from 'react';
+import Formulario from './components/Formulario';
+import Header from './components/Header';
 
 function App() {
   return (
-    <h1>
-      CLIMA
-    </h1>
+    <Fragment>
+      <Header 
+        titulo='Clima React App'
+      />
+
+      <div className="contenedor-form">
+        <div className="container">
+          <div className="row">
+            <div className="col m6 s12">
+              <Formulario />
+            </div>
+            <div className="col m6 s12">
+              2
+            </div>
+          </div>
+        </div>
+      </div>
+    </Fragment>
   );
 }
 
---
+++ src/components/Formulario.js
@@ -0,0 +1,60 @@
+import React,{ useState} from 'react'
+
+const Formulario = () => {
+
+    // STATE DEL FORMULARIO
+    const [ busqueda, setBusqueda ] = useState ({
+        ciudad: '',
+        pais: ''
+    });
+
+    // EXTRAER CIUDAD Y PAIS
+    const { ciudad, pais } = busqueda;
+
+    // FUNCION QUE COLOCA LOS ELEMENTOS EN EL STATE
+    const handleChange = e => {
+        // ACTULIZAR EL STATE
+        setBusqueda({
+            ...busqueda, 
+            [e.target.name] : e.target.value
+        });
+    }
+
+    return (
+        <form>
+            <div className="input-field col s12">
+                <input 
+                    type="text"
+                    name="ciudad"
+                    id="ciudad"
+                    value={ciudad}
+                    onChange={handleChange}
+                />
+                <label htmlFor="ciudad">Ciudad</label>
+            </div>
+            <div className="input-field col s12">
+                <select
+                    name="pais"
+                    id="pais"
+                    value={pais}
+                    onChange={handleChange}
+                >
+                    <option value="">--Seleccione un país--</option>
+                    <option value="US">Estados Unidos</option>
+                    <option value="MX">México</option>
+                    <option value="AR">Argentina</option>
+                    <option value="CO">Colombia</option>
+                    <option value="CR">Costa Rica</option>
+                    <option value="ES">España</option>
+                    <option value="PE">Perú</option>
+                    <option value="CL">Chile</option>
+
+                </select>
+                <label htmlFor="pais">País</label>
+
+            </div>
+        </form>
+    )
+}
+
+export default Formulario
