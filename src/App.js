74fbeac8f8fc4bf9aeaf899f7e9f15a36c8578f8
andgeral <geraldinebecerra93@gmal.com>
2021-12-05 19:34:30 -0300

    conexion api, errores

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
-import { Fragment } from 'react';
-import Formulario from './components/Formulario';
+import React, { Fragment, useState, useEffect } from 'react';
 import Header from './components/Header';
+import Formulario from './components/Formulario';
+import Clima from './components/Clima';
+import Error from './components/Error';
 
 function App() {
+
+    // state del formulario
+  const [busqueda, guardarBusqueda] = useState({ 
+      ciudad: '',
+      pais: ''
+  });
+  const [consultar, guardarConsultar] = useState(false);
+  const [resultado, guardarResultado] = useState({});
+  const [error, guardarError] = useState(false);
+
+  const { ciudad, pais } = busqueda;
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+
+        if(consultar) {
+          const appId = 'abade270deb3e48d82bcc1541774b676';
+          const url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
+  
+          const respuesta = await fetch(url);
+          const resultado = await respuesta.json();
+  
+          guardarResultado(resultado);
+          guardarConsultar(false);
+
+          // Detecta si hubo resultados correctos en la consulta
+
+          if(resultado.cod === "404") {
+              guardarError(true);
+          } else {
+              guardarError(false);
+          }
+        }
+        
+    }
+    consultarAPI();
+    // eslint-disable-next-line
+  },[consultar]);
+
+  let componente;
+  if(error) {
+    componente = <Error mensaje="No hay resultados" />
+  } else {
+    componente = <Clima 
+                    resultado={resultado}
+                />
+  }
   return (
     <Fragment>
-      <Header 
-        titulo='Clima React App'
-      />
+        <Header 
+          titulo='Clima React App'
+        />
 
-      <div className="contenedor-form">
-        <div className="container">
-          <div className="row">
-            <div className="col m6 s12">
-              <Formulario />
+        <div className="contenedor-form">
+            <div className="container">
+                <div className="row">
+                    <div className="col m6 s12">
+                        <Formulario 
+                          busqueda={busqueda}
+                          guardarBusqueda={guardarBusqueda}
+                          guardarConsultar={guardarConsultar}
+                        />
+                    </div>
+                    <div className="col m6 s12">
+                        {componente}
+                    </div>
+                </div>
             </div>
-            <div className="col m6 s12">
-              2
-            </div>
-          </div>
         </div>
-      </div>
     </Fragment>
   );
 }
--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,37 +1,57 @@
-import React,{ useState} from 'react'
+import React, { useState } from 'react';
+import Error from './Error';
+import PropTypes from 'prop-types';
 
-const Formulario = () => {
 
-    // STATE DEL FORMULARIO
-    const [ busqueda, setBusqueda ] = useState ({
-        ciudad: '',
-        pais: ''
-    });
+const Formulario = ({busqueda, guardarBusqueda, guardarConsultar}) => {
 
-    // EXTRAER CIUDAD Y PAIS
+
+    const [error, guardarError] = useState(false);
+
+    // extraer ciudad y pais
     const { ciudad, pais } = busqueda;
 
-    // FUNCION QUE COLOCA LOS ELEMENTOS EN EL STATE
+    // función que coloca los elementos en el state
     const handleChange = e => {
-        // ACTULIZAR EL STATE
-        setBusqueda({
-            ...busqueda, 
+        // actualizar el state
+        guardarBusqueda({
+            ...busqueda,
             [e.target.name] : e.target.value
         });
     }
 
-    return (
-        <form>
+    // Cuando el usuario da submit al form
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        // validar
+        if(ciudad.trim() === '' || pais.trim() === '') {
+            guardarError(true);
+            return;
+        }
+
+        guardarError(false);
+
+        guardarConsultar(true);
+    }
+
+    return ( 
+        <form
+            onSubmit={handleSubmit}
+        >
+            {error ? <Error mensaje="Ambos campos son obligatorios" /> : null }
+            
             <div className="input-field col s12">
-                <input 
+                <input
                     type="text"
                     name="ciudad"
                     id="ciudad"
                     value={ciudad}
                     onChange={handleChange}
                 />
-                <label htmlFor="ciudad">Ciudad</label>
+                <label htmlFor="ciudad">Ciudad: </label>
             </div>
+
             <div className="input-field col s12">
                 <select
                     name="pais"
@@ -39,7 +59,7 @@ const Formulario = () => {
                     value={pais}
                     onChange={handleChange}
                 >
-                    <option value="">--Seleccione un país--</option>
+                    <option value="">-- Seleccione un país --</option>
                     <option value="US">Estados Unidos</option>
                     <option value="MX">México</option>
                     <option value="AR">Argentina</option>
@@ -47,14 +67,24 @@ const Formulario = () => {
                     <option value="CR">Costa Rica</option>
                     <option value="ES">España</option>
                     <option value="PE">Perú</option>
-                    <option value="CL">Chile</option>
-
                 </select>
-                <label htmlFor="pais">País</label>
+                <label htmlFor="pais">País: </label>
+            </div>
 
+            <div className="input-field col s12">
+                <input  
+                    type="submit"
+                    value="Buscar Clima"
+                    className="waves-effect waves-light btn-large btn-block yellow accent-4"
+                />
             </div>
         </form>
-    )
+     );
+}
+Formulario.propTypes = {
+    busqueda : PropTypes.object.isRequired,
+    guardarBusqueda : PropTypes.func.isRequired,
+    guardarConsultar : PropTypes.func.isRequired,
 }
 
-export default Formulario
+export default Formulario;
